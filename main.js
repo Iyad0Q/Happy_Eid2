9269201ed1562ff308d4aeb20bba88711bdb5080
Iyad Al-Qassab <110106501+Iyad0Q@users.noreply.github.com>
2024-06-16 18:53:48 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 let clicker = document.querySelector('#clicker');
 let clickTimes = document.querySelector('#clickTimes');
-let codes = ['#f1j', 'r1t', 'g1w', '#f2j', 'r2t', 'g2w', '#f3j', 'r3t', 'g3w'];
-let r = codes[Math.floor(Math.random() * 9)];
+let codes = ['#f1j', 'r1t', 'g1w', 'x7h', '#f2j', 'r2t', 'g2w', 'y6x', '#f3j', 'r3t', 'g3w', 'r4tn'];
+let r = codes[Math.floor(Math.random() * 12)];
+let sheepSound = new Audio("sheep.mp3");
+let clickSound = new Audio("pop.mp3");
 let i = 1;
 
 clicker.addEventListener("click", function () {    
     if (i <= 50) {
         clickTimes.textContent = (`${i++}`);
+        clickSound.play();
     }
     else {
-        clickTimes.innerHTML = `${r} الكود:`;
+        clickTimes.textContent = `${r} الكود:`;
+        sheepSound.play();
     }
 });
 

3261362964164d56f013a1a72a21ad0ea7ce9392
Iyad Al-Qassab <110106501+Iyad0Q@users.noreply.github.com>
2024-06-16 19:57:45 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 let clicker = document.querySelector('#clicker');
 let clickTimes = document.querySelector('#clickTimes');
-let codes = ['#f1j', 'r1t', 'g1w', 'x7h', '#f2j', 'r2t', 'g2w', 'y6x', '#f3j', 'r3t', 'g3w', 'r4tn'];
+let codes = ['@f1j', '%r1t', '*g1w', '#xh', '^f2j', '#r2t', '@g2w', '#yx', '$f3j', '&r3t', '@g3w', '&f14j'];
 let r = codes[Math.floor(Math.random() * 12)];
-let sheepSound = new Audio("sheep.mp3");
-let clickSound = new Audio("pop.mp3");
+// let sheepSound = new Audio("sheep.mp3");
+// let clickSound = new Audio("pop.mp3");
 let i = 1;
 
 clicker.addEventListener("click", function () {    
     if (i <= 50) {
         clickTimes.textContent = (`${i++}`);
-        clickSound.play();
+        // clickSound.play();
     }
     else {
         clickTimes.textContent = `${r} الكود:`;
-        sheepSound.play();
+        // sheepSound.play();
     }
 });
 
